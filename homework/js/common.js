b7d182612226e25827ec3eccb81fe56f92a50e2a
approxua <kotenko1995@gmail.com>
2017-11-13 19:34:42 +0200

    rework

--- homework/js/common.js
+++ homework/js/common.js
@@ -3,42 +3,37 @@ console.log('---- Список функцій: ----');
 console.log('---- 1)findMin ---- ( Вираховує яке з вказаних чисел менше )');
 console.log('---- 2)findAr ---- ( Вираховує середнє арифметичне )');
 console.log('---- 3)findPow ---- ( Підносить до степіня)');
-var min;
-var ar;
-var pow;
+
 function findMin(a, b) {
     if (arguments.length) {
     if (a >= b) {
-        return min = b ;
+        return b ;
     } else {
-        return min = a;
+        return a;
     }
 } else {
     console.log('Введіть значення для перевірки')}
 }
 function findAr(a, b) {
     if (arguments.length) {
-        ar = (a + b) / 2;
-        return ar;
+        return (a + b) / 2;
     } else {
         console.log('Введіть значення для перевірки')}
 }
 function findPow(a, b) {
     if (arguments.length) {
         if (a && b > 0) {
-            var i;
-            pow = a;
-            for (i = 1; i < b; i++) {
-                a = a * pow;
+            var pow = a;
+            for (var i = 1; i < b; i++) {
+                pow *= a;
             }
-            return pow = a;
+            return pow;
         } else if (a === 0) {
             console.log('Число 0 до степіня не підноситься')
         } else if (a > 0 && b !== 0) {
-            pow = a * a;
-            return pow;
+            return a * a;
         } else {
-            return pow = 1;
+            return 1;
         }
     } else {
         console.log('Введіть значення')}
