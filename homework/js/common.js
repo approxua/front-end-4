820e57a5ad21cee17b954b24f8b866463aab43a8
approxua <kotenko1995@gmail.com>
2017-11-30 10:12:41 +0200

    add map

--- homework/js/common.js
+++ homework/js/common.js
@@ -3,6 +3,9 @@ function myMap() {
         center: new google.maps.LatLng(51.5, -0.12),
         zoom: 10,
         mapTypeId: google.maps.MapTypeId.HYBRID
-    }
+    };
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
+$('.slider-team').slick({
+    slidesToShow:3
+    });
